15982ead33157b33890ebe9e6a29ee527e26a0f2
Tahsin Gökalp Şaan <tahsinsaan@gmail.com>
2019-10-01 14:53:11 +0300

    Layout tamamlandı.

--- config/project.php
+++ config/project.php
@@ -8,4 +8,8 @@ return [
 
     'twitter' => env('BLOG_TWITTER', 'kodrika_yazilim'),
 
+    'copyright' => env('BLOG_COPYRIGHT', '<i class="fas fa-heart text-red-400"></i> Opensource'),
+
+    'logo' => env('BLOG_LOGO', 'true'),
+
 ];
